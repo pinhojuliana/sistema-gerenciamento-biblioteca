e2151493d086145781ce0330555020efb698455f
pinhojuliana <studyjulianaa@gmail.com>
2024-10-18 15:04:27 -0300

    implementa testes classe Autor Service

--- src/test/java/com/sistema/biblioteca/AutorServiceTest.java
+++ src/test/java/com/sistema/biblioteca/AutorServiceTest.java
@@ -1,22 +1,35 @@
 package com.sistema.biblioteca;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
+import com.sistema.biblioteca.autor.Autor;
+import com.sistema.biblioteca.autor.AutorService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AutorServiceTest {
+    @InjectMocks
+    AutorService autorService;
 
     @Test
     void deveCadastrarAutor(){
-
+        autorService.cadastrarAutor("George Orwell");
+        assertEquals("George Orwell", autorService.getAutores().get(0).getNome());
     }
 
     @Test
     void deveRetornarAutor(){
-
+        autorService.cadastrarAutor("George Orwell");
+        assertEquals("George Orwell", autorService.getAutores().get(0).getNome());
     }
 
     @Test
     void deveLancarExcecao(){
-
+        assertThrows(RuntimeException.class, ()-> {
+            autorService.verificarAutor("George Orwell");
+        });
     }
 }
