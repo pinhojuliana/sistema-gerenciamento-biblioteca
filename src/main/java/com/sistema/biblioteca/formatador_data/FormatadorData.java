6d924d223aa0573c0cd167884a0ec67df18e595c
pinhojuliana <studyjulianaa@gmail.com>
2024-10-18 15:17:22 -0300

    implementa testes classe formatador de datas

--- src/main/java/com/sistema/biblioteca/formatador_data/FormatadorData.java
+++ src/main/java/com/sistema/biblioteca/formatador_data/FormatadorData.java
@@ -2,18 +2,19 @@ package com.sistema.biblioteca.formatador_data;
 
 import org.springframework.stereotype.Component;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 //@Component verificar
 public class FormatadorData {
-    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/mm/yyyy");
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public static String formatarData(LocalDate data){
+    public static String formatarData(LocalDate data) throws DateTimeException {
         return FORMATTER.format(data);
     }
 
-    public static LocalDate formatarString(String data){
+    public static LocalDate formatarString(String data) throws DateTimeException{
         return LocalDate.parse(data, FORMATTER);
     }
 }
--- src/test/java/com/sistema/biblioteca/FormatadorDataTest.java
+++ src/test/java/com/sistema/biblioteca/FormatadorDataTest.java
@@ -1,27 +1,36 @@
 package com.sistema.biblioteca;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import static com.sistema.biblioteca.formatador_data.FormatadorData.*;
 import org.junit.jupiter.api.Test;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 public class FormatadorDataTest {
-    //dar uma olhada em como essa classe lançara excecoes
+
     @Test
     void deveRetornarStringFormatada(){
-
+        assertEquals("28/09/2005", formatarData(LocalDate.of(2005, 9, 28)));
     }
 
     @Test
     void deveRetornarExcecaoDataInvalida(){
-
+        assertThrows(DateTimeException.class, () -> {
+            formatarData(LocalDate.of(1, 1, 2007));
+        });
     }
 
     @Test
     void deveRetornarData(){
-
+        assertEquals(LocalDate.of(1960, 3, 3), formatarString("03/03/1960"));
     }
 
     @Test
     void deveRetornarExcecaoStringInvalida(){
-
+        assertThrows(DateTimeException.class, () -> {
+           formatarString("12-5-2012");
+        });
     }
 }
